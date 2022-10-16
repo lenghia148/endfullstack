aeee3a6a85dba2b466b9877dde40d03bac15db56
DESKTOP-2JOBADM\Nghia <ledinhnghia.qb@gmail.com>
2022-10-17 00:04:41 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
 import { engine } from 'express-handlebars';
 const app =  express();
 const port = 3000;
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //handlebar 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', './src/resources/views');
+app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
     //  res.send('hello world!!!')
