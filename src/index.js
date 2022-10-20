eecdf0c761c47f07aa3d7aeee2d8c3754fab0aa7
unknown <ledinhnghia.qb@gmail.com>
2022-10-20 16:54:51 +0700

    hello guy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import path from 'path';
-import {fileURLToPath} from 'url';
-
-
-import { engine } from 'express-handlebars';
+const express = require('express')
+const path = require('path')
+const hbs  = require('express-handlebars')
+const route = require('./routes')
 const app =  express();
 const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 //handlebar 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.engine('.hbs', engine({extname: '.hbs'}));
+app.engine('hbs', hbs.engine({
+    extname:".hbs"
+}))
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+//chúng ta sẽ chuyển các path qua file index này
+
+ function route(app)
+{
+
+}
+
+module.exports = route
