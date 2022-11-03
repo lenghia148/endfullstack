34ce7b6f0b3e85486d449fc360e6ef6a93b7684b
unknown <ledinhnghia.qb@gmail.com>
2022-11-03 17:55:33 +0700

    sort delete

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,26 +1,29 @@
-const HerbalTea = require('../models/HerbalTea')
-const  { mutipleMongooseToObj } = require('../../util/mongoose')
+const HerbalTea = require("../models/HerbalTea");
+const { mutipleMongooseToObj } = require("../../util/mongoose");
 
 class MeController {
-  
-//[GET] /me/stored/products
-    index(req,res,next)
-    {
-        HerbalTea.find({})
-        .then(herbalTeas=> res.render('me/stored-products',{
-            herbalTeas: mutipleMongooseToObj(herbalTeas)}))
-        .catch(next)
-    }  ;
+  //[GET] /me/stored/products
+  index(req, res, next) {
 
-    //[GET] /me/trash/products
-    trashProduct(req,res,next)
-    {
-        HerbalTea.findDeleted({})
-        .then(herbalTeas=> res.render('me/trash-products',{
-            herbalTeas: mutipleMongooseToObj(herbalTeas)}))
-        .catch(next)
-    } 
-    
-    
+    Promise.all([HerbalTea.find({}), HerbalTea.countDocumentsDeleted()])
+        .then(([herbalTeas, deletedCount]) => 
+            res.render("me/stored-products", {
+            deletedCount,
+            herbalTeas: mutipleMongooseToObj(herbalTeas),
+            })
+        )
+      .catch(next);
+  }
+
+  //[GET] /me/trash/products
+  trashProduct(req, res, next) {
+    HerbalTea.findDeleted({})
+      .then((herbalTeas) =>
+        res.render("me/trash-products", {
+          herbalTeas: mutipleMongooseToObj(herbalTeas),
+        })
+      )
+      .catch(next);
+  }
 }
-module.exports=  new MeController() ;
+module.exports = new MeController();
